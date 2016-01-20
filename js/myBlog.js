c92633b7252ae8efe2d5b4787c7c225efd913a54
sureshnswamy <sureshnswamy@gmail.com>
2016-01-20 22:18:53 +1300

    JS for next page added

--- js/myBlog.js
+++ js/myBlog.js
@@ -1,68 +1,54 @@
 
 $(document).ready (function () {
 
-var pageList = ["http://sureshnswamy.github.io/index.html",
-                "http://sureshnswamy.github.io/blog/c1-reflection-blog.html",
-                'http://sureshnswamy.github.io/blog/c1-time-and-habits-blog.html',
-                'http://sureshnswamy.github.io/blog/c2-emotional-intelligence.html',
-                "http://sureshnswamy.github.io/blog/t2-html-css-dom-p1-blog",
-                "http://sureshnswamy.github.io/blog/t2-html-css-dom-p2-blog.html",
-                "http://sureshnswamy.github.io/blog/t3-design-to-web-blog.html",
-                "http://sureshnswamy.github.io/blog/c3-meditation-process.html",
-                "http://sureshnswamy.github.io/blog/c4-daily-mindfulness.html",
-                "http://sureshnswamy.github.io/blog/t4-javascript-basics.html"
+    var pageList = ["index.html",
+                "c1-reflection-blog.html",
+                'c1-time-and-habits-blog.html',
+                'c2-emotional-intelligence.html',
+                "t2-html-css-dom-p1-blog",
+                "t2-html-css-dom-p2-blog.html",
+                "t3-design-to-web-blog.html",
+                "c3-meditation-process.html",
+                "c4-daily-mindfulness.html",
+                "t4-javascript-basics.html"
                 ];
 
-function getFileName() {
-//this gets the full url
-var url = document.location.href;
+    function getFileName(url) {
+    //this gets the full url
+    document.location=url;
 
-//this removes the anchor at the end, if there is one
-url = url.substring(0, (url.indexOf("#") == -1) ? url.length : url.indexOf("#"));
+    //this removes the anchor at the end, if there is one
+    url = url.substring(0, (url.indexOf("#") == -1) ? url.length : url.indexOf("#"));
 
-//this removes the query after the file name, if there is one
-url = url.substring(0, (url.indexOf("?") == -1) ? url.length : url.indexOf("?"));
+    //this removes the query after the file name, if there is one
+    url = url.substring(0, (url.indexOf("?") == -1) ? url.length : url.indexOf("?"));
 
-//this removes everything before the last slash in the path
-url = url.substring(url.lastIndexOf("/") + 1, url.length);
+    //this removes everything before the last slash in the path
+    url = url.substring(url.lastIndexOf("/") + 1, url.length);
 
-return url;
-}
-
-var page = getFileName(); // e.g. index.html
-var currentPosition = pageList.length // e.g. 0, 1 or 2
+    return url;
+};
 
 
-document.getElementById('next-btn').onclick = function(){
-    
-   if(currentPosition<pageList.length-1) {
-    
-    window.location = pageList[++currentPosition];
-    
-    } else {
+document.getElementById('next-btn').onclick = function next(){
 
-        window.location = pageList[0];
-    };
+    var hr = window.location.href;
+    var page = getFileName(hr);
+    var nxtIndex =  pageList.indexOf(page) + 1 ;
+    console.log(page,nxtIndex);
+    if(nxtIndex > pageList.length-1) nxtIndex=0;
+    var redirect = 'http://sureshnswamy.github.io/blog/'+pageList[nxtIndex];
+    window.location.href=redirect;
 
 };
 
-    /*if(currentPosition==-1) return;
-    
-    window.location = currentPosition<pageList.length-1? pageList[++currentPosition]:pageList[0];
-};*/
-});
-
-/*function getUrl(next) { 
-    current = document.location.href;
-    index = (next) ? 1 : -1;
-    for(i = 0;i < pageList.length;i++) {
-        if(current.toString().indexOf(pageList[i]) > -1) {
-            return (pageList[i + index]);
-        }
-    }
+function prev(){
+    var hr = window.location.href.splitOnLast("/");
+    var page=hr[1].replace("/","");
+    var nxtIndex =  pageArray.indexOf(page) - 1 ;
+    if(nxtIndex < 0) nxtIndex=pageArray.length-1;
+    var redirect = hr[0]+"/"+pageArray[nxtIndex];
+    window.location.href=redirect;
 }
 
-var nextUrl = getUrl(true);
-var prevUrl = getUrl(false);
-
-});*/
+});
