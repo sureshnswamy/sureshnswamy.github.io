9bcb12432636783323d1238e589f0bac94377779
sureshnswamy <sureshnswamy@gmail.com>
2016-01-20 23:27:42 +1300

    JS for next page edits

--- js/myBlog.js
+++ js/myBlog.js
@@ -1,16 +1,16 @@
 
 $(document).ready (function () {
 
-    var pageList = ["index.html",
-                "c1-reflection-blog.html",
-                'c1-time-and-habits-blog.html',
-                'c2-emotional-intelligence.html',
-                "t2-html-css-dom-p1-blog",
-                "t2-html-css-dom-p2-blog.html",
-                "t3-design-to-web-blog.html",
-                "c3-meditation-process.html",
-                "c4-daily-mindfulness.html",
-                "t4-javascript-basics.html"
+    var pageList = ["http://sureshnswamy.github.io/index.html",
+                "http://sureshnswamy.github.io/blog/c1-reflection-blog.html",
+                'http://sureshnswamy.github.io/blog/c1-time-and-habits-blog.html',
+                'http://sureshnswamy.github.io/blog/c2-emotional-intelligence.html',
+                "http://sureshnswamy.github.io/blog/t2-html-css-dom-p1-blog",
+                "http://sureshnswamy.github.io/blog/t2-html-css-dom-p2-blog.html",
+                "http://sureshnswamy.github.io/blog/t3-design-to-web-blog.html",
+                "http://sureshnswamy.github.io/blog/c3-meditation-process.html",
+                "http://sureshnswamy.github.io/blog/c4-daily-mindfulness.html",
+                "http://sureshnswamy.github.io/blog/t4-javascript-basics.html"
                 ];
 
     function getFileName(url) {
@@ -33,14 +33,17 @@ $(document).ready (function () {
 document.getElementById('next-btn').onclick = function next(){
 
     var hr = window.location.href;
-    var page = getFileName(hr);
-    var nxtIndex =  pageList.indexOf(page) + 1 ;
-    console.log(page,nxtIndex);
-    if(nxtIndex >=pageList.length-1) nxtIndex=0;
-    var redirect = 'http://sureshnswamy.github.io/blog/'+pageList[nxtIndex];
-    window.location.href=redirect;
-
-};
+    //var page = getFileName(hr);
+    var nxtIndex =  pageList.indexOf(hr) + 1 ;
+    console.log(hr,nxtIndex);
+   
+      if(nxtIndex >pageList.length) nxtIndex = 0;
+        var redirect = pageList[nxtIndex];
+        window.location.href=redirect;
+    
+    };
+    
+ 
 
 function prev(){
     var hr = window.location.href.splitOnLast("/");
